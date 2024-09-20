72c7154c9428450df3c7872f432c448f3684eece
Teja Annamdevula <tejaannamdevula@gmail.com>
2024-09-20 12:14:52 +0530

    feat:retreieved data from file

--- Http_server/index.js
+++ Http_server/index.js
@@ -1,21 +1,12 @@
 const http = require("http");
-const fs = require("fs");
+const fs = require("fs/promises");
 const port = 4000;
 
 const server = http.createServer(async (req, res) => {
   try {
+    const data = await fs.readFile("./index.html");
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(`<!DOCTYPE html>
-      <html lang="en">
-        <head>
-          <meta charset="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <title>Document</title>
-        </head>
-        <body>
-          <h1>Hello World!!</h1>
-        </body>
-      </html>`);
+    res.end(`${data}`);
   } catch (err) {
     console.log(err);
     throw err;
